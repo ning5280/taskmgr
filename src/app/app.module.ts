badbaa5f5e2ce802c907a92b26ddda351785ad80
wangning <529023364@qq.com>
2017-08-02 15:20:22 +0800

    设置路由并且生成简易登录页

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
-import { MdSidenavModule } from '@angular/material'
+import { LoginModule } from './login/login.module'
+import { MdSidenavModule } from '@angular/material';
+import { AppRoutingModule } from './app-routing.module'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    CoreModule,
-    MdSidenavModule
+    CoreModule, 
+    MdSidenavModule,
+    AppRoutingModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,14 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MdToolbarModule, MdIconModule, MdButtonModule} from '@angular/material';
+import { SharedModule} from '../shared/shared.module';
+import { DomSanitizer } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   imports: [
-    CommonModule,
-    MdToolbarModule,
-    MdIconModule,
-    MdButtonModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   exports:[ HeaderComponent, FooterComponent, SidebarComponent]
