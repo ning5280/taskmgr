3351b55b9855a6c8021ab456a1a55fd7c2c24499
wangning <529023364@qq.com>
2017-08-02 09:35:14 +0800

    first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class CoreModule { 
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if(parent) {
+      throw new Error('模块已经存在，不再次加载');
+    }
+  }
+}
