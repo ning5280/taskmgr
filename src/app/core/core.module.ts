10b1f1ee7fa3a3a650da4769fb3ea7fc40ac0a57
wangning <529023364@qq.com>
2017-08-02 13:42:32 +0800

    侧边栏点击闪开收缩，并且注入material图标

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
+import { MdToolbarModule, MdIconModule, MdButtonModule} from '@angular/material';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdButtonModule
   ],
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   exports:[ HeaderComponent, FooterComponent, SidebarComponent]
