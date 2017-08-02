eaa1ca5a88a14a34b4a338e2028a9afc6866bfc0
wangning <529023364@qq.com>
2017-08-02 11:01:28 +0800

    布局确定以及侧边栏

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
-
+import { MdSidenavModule } from '@angular/material'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports:[ HeaderComponent, FooterComponent, SidebarComponent]
 })
 export class CoreModule { 
   constructor(@Optional() @SkipSelf() parent: CoreModule) {
